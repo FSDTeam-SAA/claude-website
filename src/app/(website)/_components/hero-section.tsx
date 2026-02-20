3b4d6503ffe95613236d86de59a9be77db282f51
Kongkon45 <kongkon4545@gmail.com>
2026-02-21 00:23:15 +0600

    video and meta title change

--- src/app/(website)/_components/hero-section.tsx
+++ src/app/(website)/_components/hero-section.tsx
@@ -7,10 +7,20 @@ const HeroSection = () => {
       <div
         className="relative min-h-[calc(100vh)] flex items-center justify-center overflow-hidden"
       >
-
+{/* large device  */}
         <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          className="hidden md:block absolute top-0 left-0 w-full h-full object-cover"
           src="/assets/videos/hero.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+
+        {/* small device  */}
+        <video
+          className="block md:hidden absolute top-0 left-0 w-full h-full object-cover"
+          src="/assets/videos/mobile_hero.mp4"
           autoPlay
           loop
           muted
@@ -18,20 +28,11 @@ const HeroSection = () => {
         />
         <div className="absolute inset-0 bg-black/40" />
 
-        {/* <Link
-          href="/prices"
-          className="absolute bottom-36 md:bottom-20 lg:bottom-32 left-1/2 -translate-x-1/2"
-        >
-          <button className="w-[200px] md:w-[250px] h-[44px] md:h-[50px] lg:h-[56px] bg-[#E7E7E7] hover:bg-primary hover:text-white ease-in-out duration-200 transition-all py-3 px-5 md:px-6 rounded-full text-sm md:text-base lg:text-lg font-medium leading-[120%] text-primary">
-            Buy Your Data Now
-          </button>
-        </Link> */}
-
           <Link
           href="/profiles"
-          className="absolute bottom-36 md:bottom-20 lg:bottom-32 left-1/2 -translate-x-1/2"
+          className="absolute bottom-24 md:bottom-20 lg:bottom-32 left-1/2 -translate-x-1/2"
         >
-          <button className="w-[200px] md:w-[250px] h-[44px] md:h-[50px] lg:h-[56px] bg-primary ease-in-out duration-200 transition-all py-3 px-5 md:px-6 rounded-full text-sm md:text-base lg:text-lg font-medium leading-[120%] text-whhite">
+          <button className="w-[200px] md:w-[250px] h-[40px] md:h-[50px] lg:h-[56px] bg-primary ease-in-out duration-200 transition-all py-3 px-5 md:px-6 rounded-full text-sm md:text-base lg:text-lg font-medium leading-[120%] text-whhite">
             Get Your Player Report
           </button>
         </Link>
