1a5903a40ddb9ad01a78749971f14d3826376d92
Kongkon45 <kongkon4545@gmail.com>
2026-02-21 22:57:20 +0600

    problem solve

--- src/components/modals/LogoutModal.tsx
+++ src/components/modals/LogoutModal.tsx
@@ -17,15 +17,15 @@ type logoutModalProps = {
 const LogoutModal = ({ isOpen, onClose, onConfirm }: logoutModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-[420px] bg-white">
+      <DialogContent className="max-w-[90%] md:max-w-[420px] bg-white !rounded-[12px]">
         <DialogHeader>
-          <DialogTitle className="dark:text-black">Are you sure you want to log out?</DialogTitle>
-          <DialogDescription className="dark:text-black pt-1">
+          <DialogTitle className="dark:text-black text-lg md:text-xl pt-1">Are you sure you want to log out?</DialogTitle>
+          <DialogDescription className="dark:text-black text-sm md:text-base pt-1">
             You are about to log out of your account. You will need to log in
             again to continue using the services.
           </DialogDescription>
         </DialogHeader>
-        <DialogFooter className="w-full flex items-center justify-end gap-7 mt-5">
+        <DialogFooter className="w-full flex flex-row items-center justify-between gap-7 mt-3 md:mt-5">
           <button
             className="text-white  bg-black py-[8px] px-6 text-sm font-medium leading-[120%] rounded-[8px]"
             onClick={onConfirm}
