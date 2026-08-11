cdc0d2a6505b96486f3c47007e33c7abcad98ea9
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-08-11 09:02:32 +0600

    problem solve

--- src/app/(player-profile)/player-profile/[id]/_components/highlights-video.tsx
+++ src/app/(player-profile)/player-profile/[id]/_components/highlights-video.tsx
@@ -231,7 +231,7 @@ const HighlightsVideo = ({
               ? "Momentos destacados"
                 : "Highlights"}
         </h3>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {videos.map((video, index) => (
             <div
               key={`${video.originalUrl}-${index}`}
